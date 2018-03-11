aee513b7311c9398fb4ef917b721f1a34c071823
Seong-Min Kang <mail@seong.de>
2018-03-11 19:29:27 +0100

    initial bot structure

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -2,14 +2,23 @@ package bot
 
 type (
 	Bot struct {
+		ID int64
+		// x, y and angle
 		X, Y float64
 		A    float64
+		// delta V
+		DV float64
 
-		Energy  float64
+		Energy float64
+
+		IsAutotroph bool
+
 		Signals Signals
 	}
 
+	Signal [9]float64
+
 	Signals struct {
-		A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z float64
+		A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z Signal
 	}
 )
